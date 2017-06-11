3660d6254330e09ff3e40551f0e108cdb64dc5ef
Rafael <rafaeleiki@yahoo.com.br>
2017-06-11 17:55:08 -0300

    Criada a página de perfil

---
+++ js/css.js
@@ -0,0 +1,16 @@
+var loadCss;
+
+(function () {
+    'use strict';
+
+    loadCss = function () {
+        var cssFiles = [
+            'profile.css'
+        ];
+
+        cssFiles.forEach((fileName) => {
+            var linkElement = $(`<link rel="stylesheet" href="pages/css/${fileName}" />`);
+            $(document.body).append(linkElement);
+        });
+    }
+})();
